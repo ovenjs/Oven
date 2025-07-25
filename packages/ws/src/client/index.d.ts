cbbb95483182a8b8ab6dfe4c5b7d2ad067fe80a4
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-07-25 19:12:19 +0500

    Delete packages/ws/src/client/index.d.ts

--- packages/ws/src/client/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * WebSocket client exports
- */
-export { WebSocketClient } from './WebSocketClient.js';
-export type { WebSocketClientOptions, WebSocketClientStatus, WebSocketClientEvents } from './WebSocketClient.js';
-//# sourceMappingURL=index.d.ts.map
