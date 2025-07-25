7c85cab912fbe14a050c6b896f1d5b6632967b36
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-07-25 18:32:42 +0500

    Delete packages/ws/src/sharding/index.d.ts

--- packages/ws/src/sharding/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sharding exports
- */
-export { Shard, ShardState } from './Shard.js';
-export { ShardManager } from './ShardManager.js';
-export type { ShardOptions, ShardStatus } from './Shard.js';
-export type { ShardManagerOptions, ShardManagerStatus } from './ShardManager.js';
-//# sourceMappingURL=index.d.ts.map
