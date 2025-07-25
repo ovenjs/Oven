020f06936924ca42d5f23ecedc894b9f6725bf16
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-07-25 18:52:53 +0500

    Delete packages/ws/src/handlers/index.d.ts

--- packages/ws/src/handlers/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * WebSocket handlers exports
- */
-export { EventHandler } from './EventHandler.js';
-export type { EventHandlerOptions, ProcessedEvent } from './EventHandler.js';
-//# sourceMappingURL=index.d.ts.map
