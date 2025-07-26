093aba9d3c444848bd20ac858719707cb26d135e
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-07-26 21:00:00 +0000

    chore: @ovenjs/types -> add(User)

---
+++ packages/types/src/types/User.ts
@@ -0,0 +1,29 @@
+import { UserId } from "../primitives/brand";
+
+export interface User {
+    id:                 UserId;
+    username:           string;
+    discriminator:      string;
+    global_name?:       string;
+    avatar?:            string;
+    bot?:               boolean;
+    system?:            boolean;
+    mfa_enabled?:       boolean;
+    banner?:            string;
+    accent_color?:      number;
+    locale?:            string;
+    verified?:          boolean;
+    email?:             string | null;
+    flags?:             number;
+    premium_type?:      number;
+    public_flags?:      number;
+    avatar_decoration?: string;
+}
+
+export interface ClientUser extends User {
+  verified:     boolean;
+  email:        string | null;
+  flags:        number;
+  premium_type: number;
+  mfa_enabled:  boolean;
+}
